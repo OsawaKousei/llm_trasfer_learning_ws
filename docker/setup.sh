90afa6dcad987a3435bfa66ba4b48da2b20997f7
OsawaKousei <jwithelis@gmail.com>
2024-05-31 20:20:06 +0900

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,5 +3,9 @@ sudo pip install --no-cache git+https://github.com/huggingface/transformers@de92
 
 # clone ws
 cd ~/
+git clone https://github.com/OsawaKousei/llm_trasfer_learning_ws.git
+cd llm_trasfer_learning_ws
 git clone https://github.com/takaaki-inada/rinna-3.6b-hello-zundamon-ai.git
 cd rinna-3.6b-hello-zundamon-ai
+sudo rm -rf .git
+cd ~/
