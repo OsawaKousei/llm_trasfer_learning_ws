4293e19806ca993c406d922d12b98ac82188c761
OsawaKousei <jwithelis@gmail.com>
2024-05-31 20:15:31 +0900

    initialize

---
+++ docker/setup.sh
@@ -0,0 +1,7 @@
+# additional setup
+sudo pip install --no-cache git+https://github.com/huggingface/transformers@de9255de27abfcae4a1f816b904915f0b1e23cd9
+
+# clone ws
+cd ~/
+git clone https://github.com/takaaki-inada/rinna-3.6b-hello-zundamon-ai.git
+cd rinna-3.6b-hello-zundamon-ai
